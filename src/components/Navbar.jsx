93aaaec72eedc3acc743d0dc15404f1f0921a858
Your Name <you@example.com>
2025-07-29 17:39:56 +0800

    [feat] Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,19 @@
+import { useState } from "react";
+
 const Navbar = () => {
+  const [open, setOpen] =useState(false);
   return (
-    <div className="w-full h-15 md:h-20 flex items-center">
+    <div className="w-full h-16 md:h-20 flex justify-between items-center">
       {/* LOGO */}
-      <div className="">
+      <div className="flex items-center gap-4 text-2xl font-bold">
         <img src="/logo.png" className="w-8 h-8" alt="" />
         <span>CCHlog</span>
       </div>
 
       {/* MOBILE MENU */}
-      <div className="md:hidden">M</div>
+      <div className="md:hidden">
+        <div className="cursor-pointer">{open ? "X" : "="}</div>
+      </div>
       {/* DESKTOP MENU */}
       <div className="hidden md:flex">D</div>
     </div>
