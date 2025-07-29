ddec48640aa1f36ee4a4f8ca0d5a6a9f534fb5f2
CCH2655 <chengch2655@gmail.com>
2025-07-29 23:07:52 +0800

    [update] Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,10 +12,39 @@ const Navbar = () => {
 
       {/* MOBILE MENU */}
       <div className="md:hidden">
-        <div className="cursor-pointer">{open ? "X" : "="}</div>
+
+        {/* MOBILE BUTTON */}
+        <div 
+          className="cursor-pointer text-4xl" 
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          {open ? "X" : "≡"}
+        </div>
+
+        {/* MOBILE LINK LIST */}
+        <div className={`w-full h-screen flex flex-col items-center justify-center absolute top-16 transition-all ease-in-out ${
+          open ? "-right-0" : "-right-[100%]"
+          }`}
+        >
+          <a href="/">Home</a>
+          <a href="/">Trending</a>
+          <a href="/">Most Popular</a>
+          <a href="/">About</a>
+          <a href="">
+            <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white"> Login </button>
+          </a>
+        </div>
       </div>
       {/* DESKTOP MENU */}
-      <div className="hidden md:flex">D</div>
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <a href="/">Home</a>
+        <a href="/">Trending</a>
+        <a href="/">Most Popular</a>
+        <a href="/">About</a>
+        <a href="">
+          <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white"> Login </button>
+        </a>
+      </div>
     </div>
   )
 }
