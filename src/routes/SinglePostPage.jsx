1e2b5a28a20e742ae1f26e0abde9403efece8730
Your Name <you@example.com>
2025-08-04 17:46:12 +0800

    [update] SinglePostPage, PostMenuAction.

--- src/routes/SinglePostPage.jsx
+++ src/routes/SinglePostPage.jsx
@@ -1,6 +1,94 @@
+import { Link } from "react-router-dom";
+import Image from "../components/Image"
+import PostMenuAction from "../components/PostMenuAction";
+
 const SinglePostPage = () => {
   return (
-    <div className=''>SinglePostPage</div>
+    <div className="flex flex-col gap-8">
+      {/* detail */}
+      <div className="flex gap-8">
+        <div className="lg:3/5 flex flex-col gap-8">
+          <h1 className="text-xl md:text-3xl xl:text-4xl 2xl:text-5xl font-semibold">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+          </h1>
+          <div className="flex items-center gap-2 text-gray-400 text-sm">
+            <span>Written by</span>
+            <Link className="text-blue-800">someone</Link>
+            <span>on</span>
+            <Link className="text-blue-800">Web design</Link>
+            <span>2 days ago</span>
+          </div>
+
+          <p className="text-gray-500 font-medium">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Atque tempore numquam, minus quidem quibusdam suscipit quae quos eveniet dolore.
+          </p>
+
+        </div>
+        <div className="hidden lg:block w-2/5">
+          <Image src="postImg.jpeg" w="600" className="rounded-2xl"/>
+        </div>
+
+      </div>
+      {/* context */}
+      <div className="flex flex-col md:flex-row gap-8">
+
+        {/* text */}
+        <div className="lg:text-lg flex flex-col gap-6 text-justify">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ratione officia dolores hic explicabo optio magni asperiores modi ad vitae facilis quisquam, eum eius tenetur nemo odio animi veritatis doloremque.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ratione officia dolores hic explicabo optio magni asperiores modi ad vitae facilis quisquam, eum eius tenetur nemo odio animi veritatis doloremque.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ratione officia dolores hic explicabo optio magni asperiores modi ad vitae facilis quisquam, eum eius tenetur nemo odio animi veritatis doloremque.</p>
+        </div>
+        {/* menu */}
+        <div className="px-4 h-max sticky top-8">
+          <h1>Author</h1>
+          <div className="">
+            <Image
+              src="userImg.jpeg"
+              className="w-12 h-12 rounded-full object-cover"
+              w="48"
+              h="48"
+            />
+            <Link> Someone </Link>
+            <p>Lorem ipsum dolor sit amet consectetur </p>
+            <div className="flex gap-2">
+              <Link>
+                <Image src="facebook.svg"/>
+              </Link>
+              <Link>
+                <Image src="instagram.svg"/>
+              </Link>
+            </div>
+            <PostMenuAction/>
+            <h1>Categories</h1>
+            <div className="flex flex-col gap-2 text-sm">
+              <Link className="underline">All</Link>
+              <Link className="underline" to="/">
+                Web Design
+              </Link>
+              <Link className="underline" to="/">
+                Developement
+              </Link>
+              <Link className="underline" to="/">
+                Databases
+              </Link>
+              <Link className="underline" to="/">
+                Search Engines
+              </Link>
+              <Link className="underline" to="/">
+                Marketing
+              </Link>
+            </div>
+
+            <h1>Search</h1>
+
+          </div>
+
+        </div>
+
+      </div>
+
+
+    </div>
   )
 }
 
