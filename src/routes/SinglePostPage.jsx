d2e053dc950d724ccf6923be0df8ed45c574af5a
CCH2655 <chengch2655@gmail.com>
2025-08-05 21:40:38 +0800

    [update] SinglePostPage; Comments;

--- src/routes/SinglePostPage.jsx
+++ src/routes/SinglePostPage.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import Image from "../components/Image"
 import PostMenuAction from "../components/PostMenuAction";
+import Search from "../components/Search";
+import Comments from "../components/Comments";
 
 const SinglePostPage = () => {
   return (
@@ -30,26 +32,37 @@ const SinglePostPage = () => {
 
       </div>
       {/* context */}
-      <div className="flex flex-col md:flex-row gap-8">
+      <div className="flex flex-col md:flex-row gap-12">
 
         {/* text */}
         <div className="lg:text-lg flex flex-col gap-6 text-justify">
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ratione officia dolores hic explicabo optio magni asperiores modi ad vitae facilis quisquam, eum eius tenetur nemo odio animi veritatis doloremque.</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ratione officia dolores hic explicabo optio magni asperiores modi ad vitae facilis quisquam, eum eius tenetur nemo odio animi veritatis doloremque.</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ratione officia dolores hic explicabo optio magni asperiores modi ad vitae facilis quisquam, eum eius tenetur nemo odio animi veritatis doloremque.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ratione officia dolores hic explicabo optio magni asperiores modi ad vitae facilis quisquam, eum eius tenetur nemo odio animi veritatis doloremque.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ratione officia dolores hic explicabo optio magni asperiores modi ad vitae facilis quisquam, eum eius tenetur nemo odio animi veritatis doloremque.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ratione officia dolores hic explicabo optio magni asperiores modi ad vitae facilis quisquam, eum eius tenetur nemo odio animi veritatis doloremque.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ratione officia dolores hic explicabo optio magni asperiores modi ad vitae facilis quisquam, eum eius tenetur nemo odio animi veritatis doloremque.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ratione officia dolores hic explicabo optio magni asperiores modi ad vitae facilis quisquam, eum eius tenetur nemo odio animi veritatis doloremque.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ratione officia dolores hic explicabo optio magni asperiores modi ad vitae facilis quisquam, eum eius tenetur nemo odio animi veritatis doloremque.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ratione officia dolores hic explicabo optio magni asperiores modi ad vitae facilis quisquam, eum eius tenetur nemo odio animi veritatis doloremque.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ratione officia dolores hic explicabo optio magni asperiores modi ad vitae facilis quisquam, eum eius tenetur nemo odio animi veritatis doloremque.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus ratione officia dolores hic explicabo optio magni asperiores modi ad vitae facilis quisquam, eum eius tenetur nemo odio animi veritatis doloremque.</p>
         </div>
         {/* menu */}
         <div className="px-4 h-max sticky top-8">
-          <h1>Author</h1>
-          <div className="">
-            <Image
-              src="userImg.jpeg"
-              className="w-12 h-12 rounded-full object-cover"
-              w="48"
-              h="48"
-            />
-            <Link> Someone </Link>
-            <p>Lorem ipsum dolor sit amet consectetur </p>
+          <h1 className="mt-8 mb-4 text-sm font-medium">Author</h1>
+          <div className="flex flex-col gap-4">
+            <div className="flex items-center gap-8">
+              <Image
+                src="userImg.jpeg"
+                className="w-12 h-12 rounded-full object-cover"
+                w="48"
+                h="48"
+              />
+              <Link className="text-blue-800"> Someone </Link>
+            </div>
+            <p className="text-sm text-gray-500">Lorem ipsum dolor sit amet consectetur </p>
             <div className="flex gap-2">
               <Link>
                 <Image src="facebook.svg"/>
@@ -58,35 +71,34 @@ const SinglePostPage = () => {
                 <Image src="instagram.svg"/>
               </Link>
             </div>
-            <PostMenuAction/>
-            <h1>Categories</h1>
-            <div className="flex flex-col gap-2 text-sm">
-              <Link className="underline">All</Link>
-              <Link className="underline" to="/">
-                Web Design
-              </Link>
-              <Link className="underline" to="/">
-                Developement
-              </Link>
-              <Link className="underline" to="/">
-                Databases
-              </Link>
-              <Link className="underline" to="/">
-                Search Engines
-              </Link>
-              <Link className="underline" to="/">
-                Marketing
-              </Link>
-            </div>
-
-            <h1>Search</h1>
-
+          </div>
+          <PostMenuAction/>
+          <h1 className="mt-8 mb-4 text-sm font-medium">Categories</h1>
+          <div className="flex flex-col gap-2 text-sm">
+            <Link className="underline">All</Link>
+            <Link className="underline" to="/">
+              Web Design
+            </Link>
+            <Link className="underline" to="/">
+              Developement
+            </Link>
+            <Link className="underline" to="/">
+              Databases
+            </Link>
+            <Link className="underline" to="/">
+              Search Engines
+            </Link>
+            <Link className="underline" to="/">
+              Marketing
+            </Link>
           </div>
 
+          <h1 className="mt-8 mb-4 text-sm font-medium">Search</h1>
+          <Search/>
         </div>
-
       </div>
 
+      <Comments/>
 
     </div>
   )
