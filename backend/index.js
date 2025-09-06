65448bab1d2b23c493492e8c8ff49901007fc3fd
CCH2655 <chengch2655@gmail.com>
2025-09-06 14:19:20 +0800

    [feat] client: post page; backend: express init;

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
+import commentRouter from "./routes/comment.route.js";
+
+
+const app = express();
+
+console.log(process.env.test);
+
+// app.get("/test", (req,res)=>{
+//     res.status(200).send("it works!");
+// })
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+app.use("/users", commentRouter);
+
+app.listen(3000, ()=>{
+    console.log("Server is running!");
+
+})
